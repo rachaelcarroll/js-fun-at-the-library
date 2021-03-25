9b06c76bd0de55e1bc50749e502b761d63229366
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-03-24 23:12:57 -0400

    Pass createTitle and buildMainCharacter tests

--- src/book.js
+++ src/book.js
@@ -1,10 +1,20 @@
-function createTitle() {
-  
+function createTitle(bookIdea) {
+  return `The ${bookIdea}`;
+}
+createTitle("Storm's Awakening")
+
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  };
+ return character;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,

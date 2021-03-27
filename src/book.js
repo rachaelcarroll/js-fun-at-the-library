8542d08146d10170ace27a4da0ccc0e478e6e94f
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-03-26 20:43:14 -0400

    Add saveReview and calculatePageCount functions

--- src/book.js
+++ src/book.js
@@ -1,7 +1,6 @@
-function createTitle(bookIdea) {
-  return `The ${bookIdea}`;
+function createTitle(title) {
+  return `The ${title}`;
 }
-createTitle("Storm's Awakening")
 
 function buildMainCharacter(name, age, pronouns) {
   var character = {
@@ -12,11 +11,22 @@ function buildMainCharacter(name, age, pronouns) {
  return character;
 }
 
+function saveReview(content, reviews) {
+  if(!reviews.includes(content)) {
+   reviews.push(content);
+ }
+}
+
+function calculatePageCount(bookTitle) {
+  var bookPageCount = bookTitle.length * 20;
+  return bookPageCount
+}
+
 module.exports = {
   createTitle,
   buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
+  saveReview,
+  calculatePageCount,
   // writeBook,
   // editBook
 }

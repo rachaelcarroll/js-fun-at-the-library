6ef1eecefbd7136fa53a480a8c0bbe191249a174
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-03-27 17:53:37 -0400

    Complete shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,49 @@
+function shelfBook(book, shelf) {
+if (shelf.length >= 3) {
+  return shelf
+} else {
+  return shelf.unshift(book);
+}
+}
+
+function unshelfBook(title, shelf) {
+  for(var i = 0; i < shelf.length; i++) {
+    if(title === shelf[i].title) {
+     shelf.splice(i, 1);
+    }
+    }
+  }
+
+  function listTitles(shelf) {
+    var shelfTitles = [];
+    for (var i = 0; i < shelf.length; i++) {
+       shelfTitles.push(shelf[i].title);
+    }
+    return shelfTitles.join(", ");
+  }
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (title === shelf[i].title) {
+   return true;
+ }
+ }
+ return false;
+};
+
+
+
+
+
+
+
+
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };

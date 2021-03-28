07a06bc05edeb656ec95705bc14ddc46833fed30
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-03-27 22:05:25 -0400

    Pass  Library.js tests

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,56 @@
+function createLibrary(name) {
+  var library = {
+    name: name,
+    shelves: {
+    fantasy: [],
+    fiction: [],
+    nonFiction: [],
+  }
+  };
+  return library;
+}
+
+function addBook(library, book) {
+  library['shelves'][book.genre].push(book)
+};
+
+function checkoutBook(library, book, genre) {
+  for (var i = 0; i < library.shelves[genre].length; i++) {
+    var title = library.shelves[genre][i].title;
+    if (title === book) {
+      library.shelves[genre].splice(genre[i], 1);
+      return `You have now checked out ${book} from the ${library.name}`
+    }
+    }
+    return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
+   };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+createLibrary,
+addBook,
+checkoutBook
 };
